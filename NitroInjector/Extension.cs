9432c93f8b25a85dbb6b869fe84f75286b503448
REDSHADOW-TH <59532816+REDSHADOW-TH@users.noreply.github.com>
2021-09-18 03:25:19 +0700

    add extension medthods

---
+++ NitroInjector/Extension.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NitroInjector
+{
+    public static class Extension
+    {
+        public static void RegisterSingleton(this object target, string named)
+        {
+            InjectorManager.RegisterSingleton<object>(named, target);
+        }
+
+        public static void RegisterScoped(this object target, string named)
+        {
+            InjectorManager.RegisterScoped<object>(named, target);
+        }
+    }
+}
--- NitroInjector/Injector.cs
+++ NitroInjector/Injector.cs
@@ -1,25 +1,35 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace NitroInjector
 {
-    public static class NitroInjectorManager
+    public static class InjectorManager
     {
         private static Dictionary<String, object> singletonInstanceMapper = new Dictionary<string, object>();
         private static Dictionary<String, string> scopedInstanceMapper = new Dictionary<string, string>();
-        public static void RegisterSingleton<T>(string named)
+
+        public static void RegisterSingleton<T>(string named, object obj = null)
         {
+            if (obj != null)
+            {
+                singletonInstanceMapper.Add(named, obj);
+                return;
+            }
             if (singletonInstanceMapper.ContainsKey(named)) throw new Exception("instance is already exists");
             singletonInstanceMapper.Add(named, (T)Activator.CreateInstance(typeof(T)));
         }
-        public static void RegisterScoped<T>(string named)
+
+        public static void RegisterScoped<T>(string named, object obj = null)
         {
+            if (obj != null)
+            {
+                singletonInstanceMapper.Add(named, obj);
+                return;
+            }
             if (scopedInstanceMapper.ContainsKey(named)) throw new Exception("instance is already exists");
             scopedInstanceMapper.Add(named, typeof(T).FullName);
         }
+
         private static object CreateScopedInstance(string named)
         {
             if (!scopedInstanceMapper.ContainsKey(named)) throw new Exception("instance not in register scope");
@@ -32,11 +42,12 @@ namespace NitroInjector
 
             throw new Exception("instance wrong");
         }
-        public static object GetInstance(string named)
+
+        public static T GetInstance<T>(string named)
         {
-            if (singletonInstanceMapper.ContainsKey(named)) return singletonInstanceMapper[named];
-            if (scopedInstanceMapper.ContainsKey(named)) return CreateScopedInstance(named);
-            return null;
+            if (singletonInstanceMapper.ContainsKey(named)) return (T)singletonInstanceMapper[named];
+            if (scopedInstanceMapper.ContainsKey(named)) return (T)CreateScopedInstance(named);
+            throw new Exception("Not found instanse");
         }
     }
 }
